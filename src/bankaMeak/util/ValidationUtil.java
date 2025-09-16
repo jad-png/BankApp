64b94b5539ac28d389517e524d68123368aedd56
jad-png <j.belassiria@uca.ca.ma>
2025-09-16 16:57:23 +0100

    feat(): create all possible methods for validation

--- src/bankaMeak/util/ValidationUtil.java
+++ src/bankaMeak/util/ValidationUtil.java
@@ -2,4 +2,24 @@ package bankaMeak.util;
 
 public class ValidationUtil {
 	// TODO: Here in this class i need to add a method that checks the code format, positive amounts, ids, and password verification
+	public static boolean isValidCode(String code) {
+		if (code == null) return false;
+		return code.matches("^CPT-\\d{5}$");
+	}
+	
+	public static boolean isValidAmount(double montant) {
+		return montant > 0;
+	}
+	
+	public static void validateCode(String code) {
+		if (!isValidCode(code)) {
+			throw new IllegalArgumentException("Code invlide. Format attendu: CPT-00001");
+		}
+	}
+	
+	public static void validateAmount(double montant) {
+		if (!isValidAmount(montant)) {
+			throw new IllegalArgumentException("Montant invalide, il doit etre positif");
+		}
+	}
 }
