35e40270acc0715c461da485761de3577cd6942a
jad-png <j.belassiria@uca.ca.ma>
2025-09-16 11:08:04 +0100

    enhance(): just added constructor and missing attribute with getters/setters

--- src/bankaMeak/model/CompteCourant.java
+++ src/bankaMeak/model/CompteCourant.java
@@ -1,7 +1,21 @@
 package bankaMeak.model;
 
 public class CompteCourant extends Compte {
+	private double decouvert;
+	
+	public CompteCourant(int code, String userName, double decouvert) {
+		super(code, userName);
+		this.decouvert = decouvert;
+	}
 
+	public double getDecouvert() {
+		return decouvert;
+	}
+
+	public void setDecouvert(double decouvert) {
+		this.decouvert = decouvert;
+	}
+	
 	@Override
 	public void deposer(double montant) {
 		// TODO Auto-generated method stub
@@ -18,4 +32,5 @@ public class CompteCourant extends Compte {
 	public void affichierDetails() {
 		
 	}
+
 }
