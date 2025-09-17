0b08a877a8922e4a10fea623ad2e42fa96b4900b
jad-png <j.belassiria@uca.ca.ma>
2025-09-17 14:38:08 +0100

    feat(): menu to display all budget

--- src/bankaMeak/ui/ConsulterSoldeMenu.java
+++ src/bankaMeak/ui/ConsulterSoldeMenu.java
@@ -1,5 +1,25 @@
 package bankaMeak.ui;
 
-public class ConsulterSoldeMenu {
+import bankaMeak.service.*;
+import bankaMeak.util.InputUtil;
+import bankaMeak.util.ValidationUtil;
 
+public class ConsulterSoldeMenu {
+	private BankService bankSer;
+	
+	public ConsulterSoldeMenu(BankService bankSer) {
+		this.bankSer = bankSer;
+	}
+	
+	public void execute() {
+		try {
+			String code = InputUtil.readString("Saisir le code de vote compte : ");
+			ValidationUtil.validateCode(code);
+			
+			bankSer.consulterSolde(code);
+			System.out.println("test");
+		} catch (Exception e) {
+			System.out.println("Erreur : " + e.getMessage());
+		}
+	}
 }
