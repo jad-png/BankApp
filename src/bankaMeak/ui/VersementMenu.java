551f4ee3cd557fe1cc7e3962abd9c3d6a1cbbe0b
jad-png <j.belassiria@uca.ca.ma>
2025-09-17 11:22:20 +0100

    enhance(): add try/catch feature for exception

--- src/bankaMeak/ui/VersementMenu.java
+++ src/bankaMeak/ui/VersementMenu.java
@@ -12,17 +12,23 @@ public class VersementMenu {
 	}
 	
 	public void execute() {
+		try {
+			String codeSource = InputUtil.readString("Entrer votre code (format e.g: CPT-12345): ");
+			ValidationUtil.isValidCode(codeSource);
 
-		String codeSource = InputUtil.readString("Entrer votre code (format e.g: CPT-12345): ");
-		ValidationUtil.isValidCode(codeSource);
-
-		String codeDest = InputUtil.readString("Entrer le code du compte destinaire: ");
-		ValidationUtil.isValidCode(codeDest);
+			String codeDest = InputUtil.readString("Entrer le code du compte destinaire: ");
+			ValidationUtil.isValidCode(codeDest);
+			
+			Double amount = InputUtil.readDouble("Entrer le montant que vous voudrez verser: ");
+			ValidationUtil.isValidAmount(amount);
+			
+			bankSer.virement(codeSource, codeDest, amount);
+			
+			System.out.println("Versement effectue avec success");
+		} catch (Exception e) {
+			System.out.println("Erreur et servenue au niveau du versement : " + e.getMessage());
+		}
 		
-		Double amount = InputUtil.readDouble("Entrer le montant que vous voudrez verser: ");
-		ValidationUtil.isValidAmount(amount);
-		
-		bankSer.virement(codeSource, codeDest, amount);
 	}
 }
 
