46abe0db529048e76cd02a268704c44c7aca8102
jad-png <j.belassiria@uca.ca.ma>
2025-09-17 11:20:36 +0100

    enhance(): add try/catch feature for exception

--- src/bankaMeak/ui/RetraitMenu.java
+++ src/bankaMeak/ui/RetraitMenu.java
@@ -1,5 +1,32 @@
 package bankaMeak.ui;
 
-public class RetraitMenu {
+import bankaMeak.service.*;
+import bankaMeak.util.InputUtil;
+import bankaMeak.util.ValidationUtil;
 
+public class RetraitMenu {
+	BankService bankSer;
+	
+	public RetraitMenu(BankService bankService) {
+		this.bankSer = bankService;
+	}
+	
+	public void execute() {
+		try {
+			String code = InputUtil.readString("Entrer le code ud compte: ");
+			ValidationUtil.validateCode(code);
+			
+			double montant = InputUtil.readDouble("ENtrer le montant qu vous voudrer retrait: ");
+			ValidationUtil.validateAmount(montant);
+			
+			String destination = InputUtil.readString("Saisir la destination du retrait: ");
+			ValidationUtil.validateDestination(destination);
+			
+			bankSer.retrait(code, montant, destination);
+			
+			System.out.println("Retrait effectue avec succes depuis : " + destination);
+		} catch (Exception e) {
+			System.out.println("Errur est servenue : " + e.getMessage());
+		}
+	}
 }
